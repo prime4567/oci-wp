4c700fedeac4ee72fb7f231cf9d650b3e7e8571b
Benny (YC0SPU) <chandra@benny.id>
2025-04-13 21:33:52 +0700

    Add subnets. Need to review seclist

---
+++ public-subnet-seclist.tf
@@ -0,0 +1,43 @@
+# Source from https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/core_security_list
+
+resource "oci_core_security_list" "public-security-list"{
+
+  # Required
+  compartment_id = oci_identity_compartment.my_compartment.id
+  vcn_id = module.vcn.vcn_id
+
+  # Optional
+  display_name = "security-list-for-public-subnet"
+
+  
+  egress_security_rules {
+      stateless = false
+      destination = "0.0.0.0/0"
+      destination_type = "CIDR_BLOCK"
+      protocol = "all" 
+  }
+
+    ingress_security_rules { 
+      stateless = false
+      source = "0.0.0.0/0"
+      source_type = "CIDR_BLOCK"
+      # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+      protocol = "6"
+      tcp_options { 
+          min = 80
+          max = 80
+      }
+    }
+
+    ingress_security_rules { 
+        stateless = false
+        source = "0.0.0.0/0"
+        source_type = "CIDR_BLOCK"
+        # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+        protocol = "6"
+        tcp_options { 
+            min = 443
+            max = 443
+        }
+    }
+}
